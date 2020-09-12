703a82c1bfc815a00cdca461adb2380f05083611
Mio Chung <miochung7@gmail.com>
2020-09-12 16:58:32 +0100

    added js files

---
+++ js/index.js
@@ -0,0 +1,22 @@
+var typed3 = new Typed('.hero__title', {
+	strings: [
+		'hi',
+		"i'm mio chung"
+	],
+	typeSpeed: 80,
+	backSpeed: 10,
+	smartBackspace: true, // this is a default
+	loop: true
+});
+
+// var typed2 = new Typed('.hero__title', {
+// 	strings: [
+// 		'Some <i>strings</i> with',
+// 		'Some <strong>HTML</strong>',
+// 		'Chars &times; &copy;'
+// 	],
+// 	typeSpeed: 0,
+// 	backSpeed: 0,
+// 	fadeOut: true,
+// 	loop: true
+// });
